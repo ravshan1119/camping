057ce5519eedcf4e964af11741bd9465709e3ad3
ravshan <allamurodovravshan38@gmail.com>
2024-04-28 23:33:02 +0500

    assets added

---
+++ lib/core/app_colors.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  ///base color
+  static const Color black = Color(0xFF000000);
+  static const Color white = Color(0xFFFFFFFF);
+
+  ///primary color
+  static const Color green_50 = Color(0xFFF7FEE7);
+  static const Color green_100 = Color(0xFFECFCCB);
+  static const Color green_200 = Color(0xFFD9F99D);
+  static const Color green_300 = Color(0xFFBEF264);
+  static const Color green_400 = Color(0xFFA3E635);
+  static const Color green_500 = Color(0xFF84CC16);
+  static const Color green_600 = Color(0xFF65A30D);
+  static const Color green_700 = Color(0xFF4D7C0F);
+  static const Color green_800 = Color(0xFF3F6212);
+  static const Color green_900 = Color(0xFF365314);
+
+  static const Color grey_50 = Color(0xFFFAFAFA);
+  static const Color grey_100 = Color(0xFFF4F4F5);
+  static const Color grey_200 = Color(0xFFE4E4E7);
+  static const Color grey_300 = Color(0xFFD4D4D8);
+  static const Color grey_400 = Color(0xFFA1A1AA);
+  static const Color grey_500 = Color(0xFF71717A);
+  static const Color grey_600 = Color(0xFF52525B);
+  static const Color grey_700 = Color(0xFF3F3F46);
+  static const Color grey_800 = Color(0xFF27272A);
+  static const Color grey_900 = Color(0xFF18181B);
+
+  static const Color success = Color(0xFF64BC26);
+
+  static const Color warning = Color(0xFFEA1601);
+
+  static const Color info = Color(0xFFFAD202);
+}
---
+++ lib/core/app_icons.dart
@@ -0,0 +1,3 @@
+class AppIcons {
+  static const String icon = 'assets/icons/icon.png';
+}
